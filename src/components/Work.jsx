d2f5cf228d3667e057a35611062e1e1034a8d21e
zablon <zablonoigo51@gmail.com>
2024-04-05 09:34:43 +0300

    Work Component

---
+++ src/components/Work.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Work() {
+  return (
+    <>
+    <div className="relative overflow-hidden bg-[#fff6ed] lg:py-20 py-8">
+      <div className="h-[50px] bg-[#003b29] absolute -bottom-7 -left-[30px] -right-[30px] py-3 px-[30px] rounded-[50%] "></div>
+      <div className="xl:w-[1200px] mx-auto px-3 pt-8">
+        <section className="relative">
+          <p className="text-center text-[#fb7a3f]">What we do</p>
+          <h2 className="">
+            Bringing new life to the old preoceses
+          </h2>
+        </section>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Work
