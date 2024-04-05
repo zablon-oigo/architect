65fa5b53b0b9a8fae0347bf9650b0224cadb3fd9
zablon <zablonoigo51@gmail.com>
2024-04-05 12:57:35 +0300

    Adding carousel Element argument

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel'
-import "react-multi-carousel/lib/styles.css";
-
+import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 function Testimonial() {
   return (
     <>
@@ -11,7 +29,21 @@ function Testimonial() {
             <p className="">Testimonial</p>
             <div className="relative pb-8 lg:pb-0">
                 <h1 className="text-xl lg:text-4xl">What our <span className="underline decoration-[#fdca51] ">client</span> say's</h1>
-
+                <Carousel 
+                responsive={responsive}
+                swipable={true}
+                draggable={false}
+                arrows={false}
+                infinite
+                autoplay
+                renderButtonGroupOutside={true} 
+                customButtonGroup={<ButtonGroup />}
+                >
+  <div>Item 1</div>
+  <div>Item 2</div>
+  <div>Item 3</div>
+  <div>Item 4</div>
+</Carousel>
             </div>
         </div>
     </div>
