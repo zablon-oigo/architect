616210413066a40ee568fefba6c11607428195f0
zablon <zablonoigo51@gmail.com>
2024-04-05 09:39:16 +0300

    Adding Work Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Work from "./components/Work"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
     <Navbar/>
     <Hero/>
+    <Work/>
     </>
   )
 }
