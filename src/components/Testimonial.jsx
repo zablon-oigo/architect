5f824174c834bc1fecc830de8aea918beaf5218c
zablon <zablonoigo51@gmail.com>
2024-04-05 12:43:30 +0300

    Setting relative width and height

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Testimonial() {
   return (
-    <div>Testimonial</div>
+    <>
+    <div className='relative overflow-hidden bg-[#003b29] lg:py-24 py-8'>
+        <div className="h-[50px] bg-[#fff6ed] absolute -bottom-7 -left-[30px] -right-[30px] py-3 px-[30px] rounded-[50%]"></div>
+        <div className="xl:w-[1200px] text-white mx-auto px-3">
+            <p className="">Testimonial</p>
+            <div className="relative pb-8 lg:pb-0">
+                <h1 className="text-xl lg:text-4xl">What our <span className="underline decoration-[#fdca51] ">client</span> say's</h1>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
