a3b756f1cbb5aee2802b2582a0e879084142f214
zablon <zablonoigo51@gmail.com>
2024-04-06 12:12:35 +0300

    Complete testimonial with carousel

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css';
+import { FaArrowRight,FaArrowLeft } from "react-icons/fa";
+import image from '../assets/image.png'
 const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -20,7 +22,38 @@ const responsive = {
       items: 1
     }
   };
+  const Card=({name,img,title, description})=>{
+    return (
+      <div className="bg-[#06412f] w-80 relative flex justify-center z-20 p-8 mt-40">
+        <button className="rounded-full bg-[#06412f] absolute -top-20 h-32 w-64 rounded-b-none">
+        </button>
+        <div className="relative z-20 text-center">
+            <img src={img} alt="" className="w-20 h-20 mx-auto -mt-8 rounded-full" />
+            <p className="pt-4 pb-2 text-xl font-semibold">{name}</p>
+            <p className="pb-4 font-light">{title}</p>
+            <hr className="" />
+            <p className="pt-4">{description}</p>
+          </div>
+      </div>
+    )
+  }
 function Testimonial() {
+  const ButtonGroup=({next, previous})=>{
+    return(
+      <div className="absolute flex items-center justify-end w-full gap-4 mb-4 carousel-bottom-group lg:top-0 top-8">
+        <button className="block p-3 bg-slate-300 rounded-full focus:bg-[#fd7b47] hover:[#fd7b47] border border-white"
+        onClick={()=>previous()}
+        >
+        <FaArrowLeft className=""/>
+        </button>
+        <button className="block p-3 bg-slate-300 rounded-full focus:bg-[#fd7b47] hover:[#fd7b47] border border-white"
+        onClick={()=>next()}
+        >
+          <FaArrowRight className=""/>
+        </button>
+      </div>
+    )
+  }
   return (
     <>
     <div className='relative overflow-hidden bg-[#003b29] lg:py-24 py-8'>
@@ -39,11 +72,10 @@ function Testimonial() {
                 renderButtonGroupOutside={true} 
                 customButtonGroup={<ButtonGroup />}
                 >
-  <div>Item 1</div>
-  <div>Item 2</div>
-  <div>Item 3</div>
-  <div>Item 4</div>
-</Carousel>
+                   <Card img={image} title={'founder'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, incidunt?'}/>
+                   <Card img={image} title={'founder'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, incidunt?'}/>
+                   <Card img={image} title={'founder'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, incidunt?'}/>
+                </Carousel>
             </div>
         </div>
     </div>
