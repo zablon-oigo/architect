1547be0528afaaf00f2e15631968772507937042
zablon <zablonoigo51@gmail.com>
2024-04-08 14:56:16 +0300

    Changed margin on right for the image

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -52,12 +52,12 @@ function Work() {
         </section>
         <section className="relative items-center justify-between mt-12 lg:flex lg:p-8">
           <div className="flex justify-center">
-            <img src={draw} alt="" className="rounded-lg lg:w-full w-80" />
+            <img src={draw} alt="" className="mr-0 rounded-lg lg:w-full w-80 lg:mr-10" />
           </div>
           <div className="py-20 lg:w-1/2 lg:py-0">
             <p className="text-[#fb7a3f]">Our architects journey</p>
             <h1 className="text-[#054130] lg:text-5xl font-semibold pt-4 pb-6">
-              Beautiful accoustics & <span className="underline decoration-[#fdca51] ">Achievements</span> 
+              Beautiful accoustics & <span className="underline decoration-[#fdca51]">Achievements</span> 
             </h1>
             <p className="font-light leading-8 ">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime odio voluptas tempora nihil voluptatem, magnam eaque blanditiis officia iusto asperiores voluptate placeat id! Illo inventore quae veniam maxime perferendis, ad necessitatibus eum eos quo, veritatis tempore ipsa, cumque numquam! Nihil.
