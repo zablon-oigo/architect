6711e06242589a014292bc30f89c7496a0d8dc57
zablon <zablonoigo51@gmail.com>
2024-04-09 10:05:32 +0300

    Adding name argument

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css';
 import { FaArrowRight,FaArrowLeft } from "react-icons/fa";
-import image from '../assets/image.png'
+import test1 from '../assets/test1.jpg'
+import test2 from '../assets/test2.jpg'
+import test3 from '../assets/test3.jpg'
+import test4 from '../assets/test4.jpg'
 const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
@@ -22,7 +25,7 @@ const responsive = {
       items: 1
     }
   };
-  const Card=({name,img,title, description})=>{
+  const Card=({name,img,title,  description})=>{
     return (
       <div className="bg-[#06412f] w-80 relative flex justify-center z-20 p-8 mt-40">
         <button className="rounded-full bg-[#06412f] absolute -top-20 h-32 w-64 rounded-b-none">
@@ -32,7 +35,7 @@ const responsive = {
             <p className="pt-4 pb-2 text-xl font-semibold">{name}</p>
             <p className="pb-4 font-light">{title}</p>
             <hr className="" />
-            <p className="pt-4">{description}</p>
+            <p className="pt-4 font-light">{description}</p>
           </div>
       </div>
     )
@@ -73,9 +76,11 @@ function Testimonial() {
                 renderButtonGroupOutside={true} 
                 customButtonGroup={<ButtonGroup />}
                 >
-                   <Card img={image} title={'founder'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, incidunt?'}/>
-                   <Card img={image} title={'founder'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, incidunt?'}/>
-                   <Card img={image} title={'founder'} description={'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, incidunt?'}/>
+              <Card img={test1} name={'john doe'} title={'Senior Architect'} description={'Exceptional design expertise, surpassed expectations.'} />
+              <Card img={test2} name={'jane doe'} title={'Software Engineer'} description={'Innovative solutions, professional service.'} />
+              <Card img={test3} name={'alexis morgan'} title={'Doctor'} description={'Impressive attention to detail, highly recommended.'} />
+              <Card img={test4} name={'willian smith'} title={'Managing Director'} description={'Outstanding results, exceeded our vision.'} />
+
                 </Carousel>
             </div>
         </div>
